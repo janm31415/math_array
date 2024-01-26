c24c1519fa4d84d2edf060d1205f9abf113c9d5e
Jan Maes <janmaes@subadmins-iMac-2850.local>
2024-01-26 09:45:21 +0100

    custom object

--- math_array.h
+++ math_array.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <cmath>
 
 ////////////////////////////////
 // operator +
@@ -8,9 +9,9 @@
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator + (const std::array<T, dim> a, const std::array<T, dim> b) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] += b[i];
+    res[i] = a[i] + b[i];
   return res;
 }
 
@@ -31,9 +32,9 @@ std::array<T, 4> operator + (const std::array<T, 4> a, const std::array<T, 4> b)
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator + (const std::array<T, dim> a, const T s) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] += s;
+    res[i] = a[i] + s;
   return res;
 }
 
@@ -55,9 +56,9 @@ std::array<T, 4> operator + (const std::array<T, 4> a, const T s) {
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator + (const T s, const std::array<T, dim> b) {
-  std::array<T, dim> res(b);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] += s;
+    res[i] = s + b[i];
   return res;
 }
 
@@ -82,9 +83,9 @@ std::array<T, 4> operator + (const T s, const std::array<T, 4> b) {
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator - (const std::array<T, dim> a, const std::array<T, dim> b) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] -= b[i];
+    res[i] = a[i] - b[i];
   return res;
 }
 
@@ -105,9 +106,9 @@ std::array<T, 4> operator - (const std::array<T, 4> a, const std::array<T, 4> b)
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator - (const std::array<T, dim> a, const T s) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] -= s;
+    res[i] = a[i] - s;
   return res;
 }
 
@@ -156,9 +157,9 @@ std::array<T, 4> operator - (const T s, const std::array<T, 4> b) {
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator * (const std::array<T, dim> a, const std::array<T, dim> b) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] *= b[i];
+    res[i] = a[i] * b[i];
   return res;
 }
 
@@ -179,9 +180,9 @@ std::array<T, 4> operator * (const std::array<T, 4> a, const std::array<T, 4> b)
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator * (const std::array<T, dim> a, const T s) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] *= s;
+    res[i] = a[i] * s;
   return res;
 }
 
@@ -203,9 +204,9 @@ std::array<T, 4> operator * (const std::array<T, 4> a, const T s) {
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator * (const T s, const std::array<T, dim> b) {
-  std::array<T, dim> res(b);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] *= s;
+    res[i] = s * b[i];
   return res;
 }
 
@@ -230,9 +231,9 @@ std::array<T, 4> operator * (const T s, const std::array<T, 4> b) {
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator / (const std::array<T, dim> a, const std::array<T, dim> b) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] /= b[i];
+    res[i] = a[i] / b[i];
   return res;
 }
 
@@ -253,9 +254,9 @@ std::array<T, 4> operator / (const std::array<T, 4> a, const std::array<T, 4> b)
 
 template <class T, std::size_t dim>
 std::array<T, dim> operator / (const std::array<T, dim> a, const T s) {
-  std::array<T, dim> res(a);
+  std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] /= s;
+    res[i] = a[i] / s;
   return res;
 }
 
@@ -333,21 +334,108 @@ template <class T, std::size_t dim>
 std::array<T, dim> max(const std::array<T, dim> a, const std::array<T, dim> b) {
   std::array<T, dim> res;
   for (std::size_t i = 0; i < dim; ++i)
-    res[i] = a[i] > b[i] ? a[i] : b[i];
+    res[i] = a[i] < b[i] ? b[i] : a[i];
   return res;
 }
 
 template <class T>
 std::array<T, 2> max(const std::array<T, 2> a, const std::array<T, 2> b) {
-  return { a[0] > b[0] ? a[0] : b[0], a[1] > b[1] ? a[1] : b[1] };
+  return { a[0] < b[0] ? b[0] : a[0], a[1] < b[1] ? b[1] : a[1] };
 }
 
 template <class T>
 std::array<T, 3> max(const std::array<T, 3> a, const std::array<T, 3> b) {
-  return { a[0] > b[0] ? a[0] : b[0], a[1] > b[1] ? a[1] : b[1], a[2] > b[2] ? a[2] : b[2] };
+  return { a[0] < b[0] ? b[0] : a[0], a[1] < b[1] ? b[1] : a[1], a[2] < b[2] ? b[2] : a[2] };
 }
 
 template <class T>
 std::array<T, 4> max(const std::array<T, 4> a, const std::array<T, 4> b) {
-  return { a[0] > b[0] ? a[0] : b[0], a[1] > b[1] ? a[1] : b[1], a[2] > b[2] ? a[2] : b[2], a[3] > b[3] ? a[3] : b[3] };
+  return { a[0] < b[0] ? b[0] : a[0], a[1] < b[1] ? b[1] : a[1], a[2] < b[2] ? b[2] : a[2], a[3] < b[3] ? b[3] : a[3] };
+}
+
+////////////////////////////////
+// operator abs
+////////////////////////////////
+
+template <class T, std::size_t dim>
+std::array<T, dim> abs(const std::array<T, dim> a) {
+  std::array<T, dim> res(a);
+  for (std::size_t i = 0; i < dim; ++i)
+    res[i] = res[i] < 0 ? -res[i] : res[i];
+  return res;
+}
+
+template <class T>
+std::array<T, 2> abs(const std::array<T, 2> a) {
+  return { a[0] < 0 ? -a[0] : a[0], a[1] < 0 ? -a[1] : a[1]};
+}
+
+template <class T>
+std::array<T, 3> abs(const std::array<T, 3> a) {
+  return { a[0] < 0 ? -a[0] : a[0], a[1] < 0 ? -a[1] : a[1], a[2] < 0 ? -a[2] : a[2]};
+}
+
+template <class T>
+std::array<T, 4> abs(const std::array<T, 4> a) {
+  return { a[0] < 0 ? -a[0] : a[0], a[1] < 0 ? -a[1] : a[1], a[2] < 0 ? -a[2] : a[2], a[3] < 0 ? -a[3] : a[3]};
+}
+
+template <std::size_t dim>
+std::array<float, dim> abs(const std::array<float, dim> a) {
+  std::array<float, dim> res;
+  for (std::size_t i = 0; i < dim; ++i)
+    res[i] = fabs(a[i]);
+  return res;
+}
+
+inline std::array<float, 2> abs(const std::array<float, 2> a) {
+  return { fabs(a[0]), fabs(a[1])};
+}
+
+inline std::array<float, 3> abs(const std::array<float, 3> a) {
+  return { fabs(a[0]), fabs(a[1]), fabs(a[2])};
+}
+
+inline std::array<float, 4> abs(const std::array<float, 4> a) {
+  return { fabs(a[0]), fabs(a[1]), fabs(a[2]), fabs(a[3])};
+}
+
+template <std::size_t dim>
+std::array<double, dim> abs(const std::array<double, dim> a) {
+  std::array<double, dim> res;
+  for (std::size_t i = 0; i < dim; ++i)
+    res[i] = fabs(a[i]);
+  return res;
+}
+
+inline std::array<double, 2> abs(const std::array<double, 2> a) {
+  return { fabs(a[0]), fabs(a[1])};
+}
+
+inline std::array<double, 3> abs(const std::array<double, 3> a) {
+  return { fabs(a[0]), fabs(a[1]), fabs(a[2])};
+}
+
+inline std::array<double, 4> abs(const std::array<double, 4> a) {
+  return { fabs(a[0]), fabs(a[1]), fabs(a[2]), fabs(a[3])};
+}
+
+template <std::size_t dim>
+std::array<long double, dim> abs(const std::array<long double, dim> a) {
+  std::array<long double, dim> res;
+  for (std::size_t i = 0; i < dim; ++i)
+    res[i] = fabs(a[i]);
+  return res;
+}
+
+inline std::array<long double, 2> abs(const std::array<long double, 2> a) {
+  return { fabs(a[0]), fabs(a[1])};
+}
+
+inline std::array<long double, 3> abs(const std::array<long double, 3> a) {
+  return { fabs(a[0]), fabs(a[1]), fabs(a[2])};
+}
+
+inline std::array<long double, 4> abs(const std::array<long double, 4> a) {
+  return { fabs(a[0]), fabs(a[1]), fabs(a[2]), fabs(a[3])};
 }
--- tests/math_array_tests.cpp
+++ tests/math_array_tests.cpp
@@ -8,23 +8,85 @@
 namespace
 {
 
+  struct custom_object
+    {
+      custom_object() : _value(0) {}
+  
+      custom_object(double value) : _value(value) {}
+            
+      bool operator == (const custom_object& other) const
+        {
+        return other._value == _value;
+        }
+      bool operator < (const custom_object& other) const
+        {
+        return other._value < _value;
+        }
+        /*
+      bool operator >= (const custom_object& other) const
+        {
+        return !(*this < other);
+        }
+      bool operator > (const custom_object& other) const
+        {
+        return other._value > _value;
+        }
+      bool operator <= (const custom_object& other) const
+        {
+        return !(*this > other);
+        }*/
+      bool operator != (const custom_object& other) const
+        {
+        return !(other == *this);
+        }
+      custom_object operator - () const
+        {
+        return custom_object(-_value);
+        }
+      custom_object operator + (const custom_object& other) const
+        {
+        return custom_object(_value + other._value);
+        }
+      custom_object operator - (const custom_object& other) const
+        {
+        return custom_object(_value - other._value);
+        }
+      custom_object operator * (const custom_object& other) const
+        {
+        return custom_object(_value * other._value);
+        }
+      custom_object operator / (const custom_object& other) const
+        {
+        return custom_object(_value / other._value);
+        }
+      double _value;
+    };
+    
+    std::ostream& operator <<(std::ostream& stream, const custom_object& obj)
+      {
+      stream << obj._value;
+      return stream;
+      }
+
   template <class T>
   struct math_array_fixture
   {
     math_array_fixture()
     {
-      a2 = { { 1,4} };
-      b2 = { { 3,2} };
-      a3 = { { 1,2,6} };
-      b3 = { { 4,5,3} };
-      a4 = { { 1,2,3,8 } };
-      b4 = { { 5,6,7,4} };
+      a2 = { { -1,4} };
+      b2 = { { 3,-2} };
+      a3 = { { -1,2,6} };
+      b3 = { { 4,-5,3} };
+      a4 = { { -1,2,3,8 } };
+      b4 = { { 5,-6,7,4} };
       for (int i = 0; i < NDIM; ++i)
       {
         an[i] = static_cast<T>(i + 1);
         bn[i] = static_cast<T>(NDIM + i + 1);
       }
       auto tmp = bn[NDIM - 1];
+      an[0] = -an[0];
+      bn[1] = -bn[1];
       bn[NDIM - 1] = an[NDIM - 1];
       an[NDIM - 1] = tmp;
     }
@@ -59,29 +121,29 @@ namespace
 
       res2 = this->a2 + static_cast<T>(2);
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(this->a2[i] + 2, res2[i]);
+        TEST_EQ(this->a2[i] + static_cast<T>(2), res2[i]);
       res3 = this->a3 + static_cast<T>(2);
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(this->a3[i] + 2, res3[i]);
+        TEST_EQ(this->a3[i] + static_cast<T>(2), res3[i]);
       res4 = this->a4 + static_cast<T>(2);
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(this->a4[i] + 2, res4[i]);
+        TEST_EQ(this->a4[i] + static_cast<T>(2), res4[i]);
       resn = this->an + static_cast<T>(2);
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(this->an[i] + 2, resn[i]);
+        TEST_EQ(this->an[i] + static_cast<T>(2), resn[i]);
 
       res2 = static_cast<T>(2) + this->b2;
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(2 + this->b2[i], res2[i]);
+        TEST_EQ(static_cast<T>(2) + this->b2[i], res2[i]);
       res3 = static_cast<T>(2) + this->b3;
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(2 + this->b3[i], res3[i]);
+        TEST_EQ(static_cast<T>(2) + this->b3[i], res3[i]);
       res4 = static_cast<T>(2) + this->b4;
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(2 + this->b4[i], res4[i]);
+        TEST_EQ(static_cast<T>(2) + this->b4[i], res4[i]);
       resn = static_cast<T>(2) + this->bn;
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(2 + this->bn[i], resn[i]);
+        TEST_EQ(static_cast<T>(2) + this->bn[i], resn[i]);
     }
   };
 
@@ -105,29 +167,29 @@ namespace
 
       res2 = this->a2 - static_cast<T>(2);
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(this->a2[i] - 2, res2[i]);
+        TEST_EQ(this->a2[i] - static_cast<T>(2), res2[i]);
       res3 = this->a3 - static_cast<T>(2);
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(this->a3[i] - 2, res3[i]);
+        TEST_EQ(this->a3[i] - static_cast<T>(2), res3[i]);
       res4 = this->a4 - static_cast<T>(2);
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(this->a4[i] - 2, res4[i]);
+        TEST_EQ(this->a4[i] - static_cast<T>(2), res4[i]);
       resn = this->an - static_cast<T>(2);
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(this->an[i] - 2, resn[i]);
+        TEST_EQ(this->an[i] - static_cast<T>(2), resn[i]);
 
       res2 = static_cast<T>(2) - this->b2;
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(2 - this->b2[i], res2[i]);
+        TEST_EQ(static_cast<T>(2) - this->b2[i], res2[i]);
       res3 = static_cast<T>(2) - this->b3;
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(2 - this->b3[i], res3[i]);
+        TEST_EQ(static_cast<T>(2) - this->b3[i], res3[i]);
       res4 = static_cast<T>(2) - this->b4;
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(2 - this->b4[i], res4[i]);
+        TEST_EQ(static_cast<T>(2) - this->b4[i], res4[i]);
       resn = static_cast<T>(2) - this->bn;
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(2 - this->bn[i], resn[i]);
+        TEST_EQ(static_cast<T>(2) - this->bn[i], resn[i]);
     }
   };
 
@@ -151,29 +213,29 @@ namespace
 
       res2 = this->a2 * static_cast<T>(2);
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(this->a2[i] * 2, res2[i]);
+        TEST_EQ(this->a2[i] * static_cast<T>(2), res2[i]);
       res3 = this->a3 * static_cast<T>(2);
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(this->a3[i] * 2, res3[i]);
+        TEST_EQ(this->a3[i] * static_cast<T>(2), res3[i]);
       res4 = this->a4 * static_cast<T>(2);
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(this->a4[i] * 2, res4[i]);
+        TEST_EQ(this->a4[i] * static_cast<T>(2), res4[i]);
       resn = this->an * static_cast<T>(2);
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(this->an[i] * 2, resn[i]);
+        TEST_EQ(this->an[i] * static_cast<T>(2), resn[i]);
 
       res2 = static_cast<T>(2) * this->b2;
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(2 * this->b2[i], res2[i]);
+        TEST_EQ(static_cast<T>(2) * this->b2[i], res2[i]);
       res3 = static_cast<T>(2) * this->b3;
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(2 * this->b3[i], res3[i]);
+        TEST_EQ(static_cast<T>(2) * this->b3[i], res3[i]);
       res4 = static_cast<T>(2) * this->b4;
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(2 * this->b4[i], res4[i]);
+        TEST_EQ(static_cast<T>(2) * this->b4[i], res4[i]);
       resn = static_cast<T>(2) * this->bn;
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(2 * this->bn[i], resn[i]);
+        TEST_EQ(static_cast<T>(2) * this->bn[i], resn[i]);
     }
   };
 
@@ -197,29 +259,29 @@ namespace
 
       res2 = this->a2 / static_cast<T>(2);
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(this->a2[i] / 2, res2[i]);
+        TEST_EQ(this->a2[i] / static_cast<T>(2), res2[i]);
       res3 = this->a3 / static_cast<T>(2);
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(this->a3[i] / 2, res3[i]);
+        TEST_EQ(this->a3[i] / static_cast<T>(2), res3[i]);
       res4 = this->a4 / static_cast<T>(2);
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(this->a4[i] / 2, res4[i]);
+        TEST_EQ(this->a4[i] / static_cast<T>(2), res4[i]);
       resn = this->an / static_cast<T>(2);
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(this->an[i] / 2, resn[i]);
+        TEST_EQ(this->an[i] / static_cast<T>(2), resn[i]);
 
       res2 = static_cast<T>(2) / this->b2;
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(2 / this->b2[i], res2[i]);
+        TEST_EQ(static_cast<T>(2) / this->b2[i], res2[i]);
       res3 = static_cast<T>(2) / this->b3;
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(2 / this->b3[i], res3[i]);
+        TEST_EQ(static_cast<T>(2) / this->b3[i], res3[i]);
       res4 = static_cast<T>(2) / this->b4;
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(2 / this->b4[i], res4[i]);
+        TEST_EQ(static_cast<T>(2) / this->b4[i], res4[i]);
       resn = static_cast<T>(2) / this->bn;
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(2 / this->bn[i], resn[i]);
+        TEST_EQ(static_cast<T>(2) / this->bn[i], resn[i]);
     }
   };
 
@@ -231,16 +293,16 @@ namespace
     {
       auto res2 = min(this->a2,this->b2);
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(std::min<T>(this->a2[i], this->b2[i]), res2[i]);
+        TEST_EQ(this->a2[i] < this->b2[i] ? this->a2[i] : this->b2[i], res2[i]);
       auto res3 = min(this->a3, this->b3);
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(std::min<T>(this->a3[i], this->b3[i]), res3[i]);
+        TEST_EQ(this->a3[i] < this->b3[i] ? this->a3[i] : this->b3[i], res3[i]);
       auto res4 = min(this->a4, this->b4);
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(std::min<T>(this->a4[i], this->b4[i]), res4[i]);
+        TEST_EQ(this->a4[i] < this->b4[i] ? this->a4[i] : this->b4[i], res4[i]);
       auto resn = min(this->an, this->bn);
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(std::min<T>(this->an[i], this->bn[i]), resn[i]);
+        TEST_EQ(this->an[i] < this->bn[i] ? this->an[i] : this->bn[i], resn[i]);
     }
   };
 
@@ -251,18 +313,38 @@ namespace
     {
       auto res2 = max(this->a2, this->b2);
       for (int i = 0; i < 2; ++i)
-        TEST_EQ(std::max<T>(this->a2[i], this->b2[i]), res2[i]);
+        TEST_EQ(this->a2[i] < this->b2[i] ? this->b2[i] : this->a2[i], res2[i]);
       auto res3 = max(this->a3, this->b3);
       for (int i = 0; i < 3; ++i)
-        TEST_EQ(std::max<T>(this->a3[i], this->b3[i]), res3[i]);
+        TEST_EQ(this->a3[i] < this->b3[i] ? this->b3[i] : this->a3[i], res3[i]);
       auto res4 = max(this->a4, this->b4);
       for (int i = 0; i < 4; ++i)
-        TEST_EQ(std::max<T>(this->a4[i], this->b4[i]), res4[i]);
+        TEST_EQ(this->a4[i] < this->b4[i] ? this->b4[i] : this->a4[i], res4[i]);
       auto resn = max(this->an, this->bn);
       for (int i = 0; i < NDIM; ++i)
-        TEST_EQ(std::max<T>(this->an[i], this->bn[i]), resn[i]);
+        TEST_EQ(this->an[i] < this->bn[i] ? this->bn[i] : this->an[i], resn[i]);
     }
   };
+  
+  template <class T>
+  struct abs_vectors : public math_array_fixture<T>
+  {
+    void test()
+    {
+      auto res2 = abs(this->a2);
+      for (int i = 0; i < 2; ++i)
+        TEST_EQ(this->a2[i] < 0 ? -this->a2[i] : this->a2[i], res2[i]);
+      auto res3 = abs(this->a3);
+      for (int i = 0; i < 3; ++i)
+        TEST_EQ(this->a3[i] < 0 ? -this->a3[i] : this->a3[i], res3[i]);
+      auto res4 = abs(this->a4);
+      for (int i = 0; i < 4; ++i)
+        TEST_EQ(this->a4[i] < 0 ? -this->a4[i] : this->a4[i], res4[i]);
+      auto resn = abs(this->an);
+      for (int i = 0; i < NDIM; ++i)
+        TEST_EQ(this->an[i] < 0 ? -this->an[i] : this->an[i], resn[i]);
+    }
+  };  
   
   template <class T>
   void run_typed_math_array_tests()
@@ -273,6 +355,7 @@ namespace
     divide_vectors<T>().test();
     min_vectors<T>().test();
     max_vectors<T>().test();
+    abs_vectors<T>().test();
   }
 }
 
@@ -281,4 +364,5 @@ void run_math_array_tests()
   run_typed_math_array_tests<float>();
   run_typed_math_array_tests<double>();
   run_typed_math_array_tests<int>();
+  run_typed_math_array_tests<custom_object>();
 }
